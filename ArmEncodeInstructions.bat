375bfe29836cae26af1428adc1e9c5701ad492f9
mmand <matthewa054@gmail.com>
2026-04-15 18:03:16 -0400

    comment xxd and bin parts dont want em rn

--- ArmEncodeInstructions.bat
+++ ArmEncodeInstructions.bat
@@ -1,5 +1,5 @@
 arm-none-eabi-as -mcpu=arm7tdmi -o arm.o arm.s
 arm-none-eabi-ld -Ttext=0x02000000 -o arm.elf arm.o
 arm-none-eabi-objdump -d arm.elf
-arm-none-eabi-objcopy -O binary arm.elf arm.bin
-xxd -g 1 arm.bin
+REM arm-none-eabi-objcopy -O binary arm.elf arm.bin
+REM xxd -g 1 arm.bin
--- ThumbEncodeInstructions.bat
+++ ThumbEncodeInstructions.bat
@@ -1,5 +1,5 @@
 arm-none-eabi-as -mcpu=arm7tdmi -mthumb -o thumb.o thumb.s
 arm-none-eabi-ld -Ttext=0x02000000 -o thumb.elf thumb.o
 arm-none-eabi-objdump -d thumb.elf
-arm-none-eabi-objcopy -O binary thumb.elf thumb.bin
-xxd -g 1 thumb.bin
+REM arm-none-eabi-objcopy -O binary thumb.elf thumb.bin
+REM xxd -g 1 thumb.bin
